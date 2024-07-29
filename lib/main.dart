9643ed40a0645ca54747b0181fda108c7b8b20d4
moury12 <tanzibamouri00@gmail.com>
2024-07-29 11:21:27 +0600

    feat:scrollable screenshot

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:flutter/cupertino.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:flutter/material.dart';
 import 'package:screenshot/screenshot.dart';
@@ -43,7 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
   final ScreenshotController screenshotController = ScreenshotController();
-
+  List<Uint8List> images = [];
   @override
   void initState() {
     super.initState();
@@ -51,15 +52,24 @@ class _MyHomePageState extends State<MyHomePage> {
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..loadRequest(Uri.parse('https://flutter.dev'));
   }
+Future<void> captureAndSaveScreenshots() async{
+    try{
+      await captureScrollableContent();
+      await convertImageToPdf(images);
+    }catch(e){
 
+    }
+}
 
-  Future<void> convertImageToPdf(Uint8List image) async {
+  Future<void> convertImageToPdf(List<Uint8List> images) async {
     final pdf = pw.Document();
-    final imageProvider = pw.MemoryImage(image);
+    for(var image in images){
+      final imageProvider = pw.MemoryImage(image);
 
-    pdf.addPage(pw.Page(build: (pw.Context context) {
-      return pw.Center(child: pw.Image(imageProvider));
-    }));
+      pdf.addPage(pw.Page(build: (pw.Context context) {
+        return pw.Center(child: pw.Image(imageProvider));
+      }));
+    }
 
     Directory dir = Directory('/storage/emulated/0/Download');
     final file = File('${dir.path}/webview_screenshot.pdf');
@@ -73,23 +83,44 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text('Flutter Simple Example'),
       ),
-      body: Screenshot( controller: screenshotController,
-          child: RepaintBoundary(key: _globalKey,
-              child: WebViewWidget(controller: controller))),
+      body: RepaintBoundary(key: _globalKey,
+          child: SizedBox(  height: 2250, // Important Adjust the height as needed
+              width: MediaQuery.of(context).size.width,
+
+              child:  LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+
+                    return WebViewWidget(controller: controller);
+                }
+              ))),
       floatingActionButton: FloatingActionButton(
         onPressed: () async{
           // Uint8List? imageBytes = await screenshotController.capture();
-          String? base64String =
-          await ScrollScreenshot.captureAndSaveScreenshot(_globalKey);
-          // Convert imageBytes to PDF
-          Uint8List imageBytes = Uint8List.fromList(base64Decode(base64String!));
-          await convertImageToPdf(imageBytes);
+          captureAndSaveScreenshots();
+          images.clear();
         },
         tooltip: 'Download',
         child: const Icon(Icons.save),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+ Future<void> captureScrollableContent() async{
+    double scrollPosition =0;
+    bool reachend = false;
+    while(!reachend){
+      String? base64String =
+      await ScrollScreenshot.captureAndSaveScreenshot(_globalKey);
+      Uint8List imageBytes = Uint8List.fromList(base64Decode(base64String!));
+      images.add(imageBytes);
+      double maxScrollExtent =MediaQuery.of(context).size.height;
+      scrollPosition +=maxScrollExtent;
+      reachend=(scrollPosition>=maxScrollExtent);
+      if(!reachend){
+        await Future.delayed(Duration(milliseconds: 500));
+      }
+    }
+ }
 
 
 }
